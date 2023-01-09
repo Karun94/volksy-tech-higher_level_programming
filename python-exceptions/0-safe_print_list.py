f21c8ff4d7a30db8f632b050b106c5e16e3dd42d
root <karun94>
2023-01-09 04:53:45 -0800

    0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    e = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[e], end="")
+            e += 1
+            except:
+                pass
+    print('')
+    return e
