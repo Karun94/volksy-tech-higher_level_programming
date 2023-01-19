c7ef55e677b3cf4b770392742ca9f325a6b9d4a1
root <karun94>
2023-01-18 20:39:55 -0800

    6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""json"""
+
+
+def load_from_json_file(filename):
+    """create object"""
+    with open(filename, mode='r', encoding="utf-8") as f:
+        return(json.load(f))
