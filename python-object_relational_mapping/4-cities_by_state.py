a688e8abd4ae5217d11add347a81fc6932ad488d
root <karun94>
2023-02-08 01:42:43 -0800

    4-cities_by_state.py

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+""" print cities """
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    c = conn.cursor()
+    c.execute("SELECT cities.id, cities.name, states.name from cities" +
+              " INNER JOIN states ON cities.state_id = states.id ORDER BY" +
+              " cities.id ASC")
+    a = c.fetchall()
+    for i in a:
+        print(i)
+    c.close()
+    conn.close()
